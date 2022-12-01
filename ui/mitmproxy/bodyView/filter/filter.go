8d0da8b9e03cae94048722b9ba615233fd965bab
Admin <2762713521@qq.com>
2022-11-30 20:18:11 -0800

    变更

--- ui/mitmproxy/bodyView/filter/filter.go
+++ ui/mitmproxy/bodyView/filter/filter.go
@@ -1,45 +1,60 @@
 package filter
 
-import "github.com/richardwilkes/unison"
+import (
+	"github.com/richardwilkes/toolbox/i18n"
+	"github.com/richardwilkes/unison"
+)
 
-func CreateFilter() *unison.Panel {
-	panel := unison.NewPanel()
-	panel.SetLayout(&unison.FlexLayout{
-		Columns:      10,
-		HSpacing:     unison.StdHSpacing,
-		VSpacing:     unison.StdVSpacing,
-		EqualColumns: true,
-		//HAlign:       unison.StartAlignment,
-	})
-	field := unison.NewField()
-	field.SetLayoutData(&unison.FlexLayoutData{
-		HSpan:  1,
-		VSpan:  1,
+func CreateFilter() *unison.Field {
+	filter := i18n.Text("Content Filter")
+	filterField := unison.NewField()
+	filterField.Watermark = filter
+	filterField.Tooltip = unison.NewTooltipWithText(filter)
+	//filterField.ModifiedCallback = applyFilter
+	filterField.SetLayoutData(&unison.FlexLayoutData{
 		HAlign: unison.FillAlignment,
 		VAlign: unison.MiddleAlignment,
 		HGrab:  true,
 	})
-	field.Tooltip = unison.NewTooltipWithText("filter")
-	field.ModifiedCallback = func() {
-		println(field.Text())
-	}
-	panel.AddChild(field)
+	return filterField
 
-	spacer := &unison.Panel{}
-	spacer.Self = spacer
-	spacer.SetSizer(func(_ unison.Size) (min, pref, max unison.Size) {
-		min.Width = 40
-		pref.Width = 40
-		max.Width = 40
-		return
-	})
-	panel.AddChild(spacer)
-
-	//createCheckBox("RequestCtx", unison.OffCheckState, panel)
-	//createCheckBox("ResponseCtx", unison.OffCheckState, panel)
-	//createCheckBox("RequestBody", unison.OffCheckState, panel)
-	//createCheckBox("ResponseBody", unison.OffCheckState, panel)
-	//createCheckBox("Notes", unison.OffCheckState, panel)
-	//createCheckBox("Scheme", unison.OffCheckState, panel)
-	return panel
+	//panel := unison.NewPanel()
+	//panel.SetLayout(&unison.FlexLayout{
+	//	Columns:      1,
+	//	HSpacing:     unison.StdHSpacing,
+	//	VSpacing:     unison.StdVSpacing,
+	//	EqualColumns: true,
+	//	//HAlign:       unison.StartAlignment,
+	//})
+	//field := unison.NewField()
+	//field.SetLayoutData(&unison.FlexLayoutData{
+	//	HSpan:  1,
+	//	VSpan:  1,
+	//	HAlign: unison.FillAlignment,
+	//	VAlign: unison.MiddleAlignment,
+	//	HGrab:  true,
+	//})
+	//field.Tooltip = unison.NewTooltipWithText("filter")
+	//field.ModifiedCallback = func() {
+	//	println(field.Text())
+	//}
+	//panel.AddChild(field)
+	//
+	//spacer := &unison.Panel{}
+	//spacer.Self = spacer
+	//spacer.SetSizer(func(_ unison.Size) (min, pref, max unison.Size) {
+	//	min.Width = 140
+	//	pref.Width = 140
+	//	max.Width = 140
+	//	return
+	//})
+	//panel.AddChild(spacer)
+	//
+	////createCheckBox("RequestCtx", unison.OffCheckState, panel)
+	////createCheckBox("ResponseCtx", unison.OffCheckState, panel)
+	////createCheckBox("RequestBody", unison.OffCheckState, panel)
+	////createCheckBox("ResponseBody", unison.OffCheckState, panel)
+	////createCheckBox("Notes", unison.OffCheckState, panel)
+	////createCheckBox("Scheme", unison.OffCheckState, panel)
+	//return panel
 }
