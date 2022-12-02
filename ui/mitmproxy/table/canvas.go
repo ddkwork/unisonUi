ebc6de5f5401e0da7c625686aacaee7435a0acf5
Admin <2762713521@qq.com>
2022-12-02 06:13:37 -0800

    变更

--- ui/mitmproxy/table/canvas.go
+++ ui/mitmproxy/table/canvas.go
@@ -44,95 +44,17 @@ type (
 
 func New() *object { return &object{} }
 func CanvasObject(w *unison.Window) (ok bool) {
-	//return CanvasObject_(w)
 	menus.New(w)
 	content := w.Content()
 	content.SetLayout(&unison.FlexLayout{Columns: 1})
 	content.AddChild(toolbar.CreateToolBar())
-
-	go func() {
-		ux.RegisterKnownFileTypes()
-		ux.NewWorkspace(w)
-		f := "Steward.gct"
-		dockable, open := ux.OpenFile(w, f)
-		if !open {
-			mylog.Error("!open")
-			return
-		}
-		content.AddChild(dockable)
-		//content.AddChild(CreatTable())  
-	}()
-
+	n := "Thaumatology - RPM Advantage Modifiers.adm"
+	noteTableDockableFromFile, err := ux.NewTraitModifierTableDockableFromFile(n)
+	if !mylog.Error(err) {
+		return
+	}
+	content.AddChild(noteTableDockableFromFile)
 	content.AddChild(filter.CreateFilter())
 	content.AddChild(bodyView.CreateBodyView())
-	return true
-}
-
-func CanvasObject_(w *unison.Window) (ok bool) {
-	o := &object{
-		Object:  packets.Object{},
-		packets: nil,
-		table:   unison.NewTable[*object](&unison.SimpleTableModel[*object]{}),
-		parent:  nil,
-		id:      uuid.UUID{},
-		//id:           uuid.UUID{},
-		treeIdOrSub:  "",
-		tips:         "",
-		root:         make([]*object, 0),
-		branch:       make([]*object, 0),
-		checkbox:     nil,
-		container:    false,
-		open:         false,
-		doubleHeight: false,
-		RWMutex:      sync.RWMutex{},
-	}
-	o.table.ShowRowDivider = false
-	o.table.HierarchyColumnIndex = 1
-	o.table.ColumnSizes = make([]unison.ColumnSize, len(o.Header())+1)
-	for i := range o.table.ColumnSizes {
-		o.table.ColumnSizes[i].Minimum = 20
-		o.table.ColumnSizes[i].Maximum = 10000
-	}
-	_, checkColSize, _ := unison.NewCheckBox().Sizes(unison.Size{})
-	o.table.ColumnSizes[0].Minimum = checkColSize.Width
-	o.table.ColumnSizes[0].Maximum = checkColSize.Width
-
-	o.Table().SelectionChangedCallback = func() {
-		//todo get packet to table page
-	}
-	o.Table().SetRootRows(o.root)
-	o.Table().SizeColumnsToFit(true)
-	o.Table().InstallDragSupport(nil, "object", "Row", "Column")
-	unison.InstallDropSupport[*object, any](o.Table(), "object", func(from, to *unison.Table[*object]) bool { return from == to }, nil, nil)
-
-	o.Table().SetRootRows(o.root)
-	o.Table().SizeColumnsToFit(true)
-	o.Table().InstallDragSupport(nil, "object", "Row", "Column")
-	unison.InstallDropSupport[*object, any](o.Table(), "object", func(from, to *unison.Table[*object]) bool { return from == to }, nil, nil)
-
-	go o.mitmMock()
-	header := unison.NewTableHeader[*object](o.Table(), unison.NewTableColumnHeader[*object]("", "")) //check
-	for _, s := range o.Header() {
-		header.ColumnHeaders = append(header.ColumnHeaders, unison.NewTableColumnHeader[*object](s, "")) //add header
-	}
-	header.SetLayoutData(&unison.FlexLayoutData{
-		HAlign: unison.FillAlignment,
-		VAlign: unison.FillAlignment,
-		HGrab:  true,
-	})
-	content := w.Content()
-	content.SetLayout(&unison.FlexLayout{Columns: 1})
-	content.AddChild(header)
-
-	// Create a scroll panel and tips a table panel inside it
-	scrollArea := unison.NewScrollPanel()
-	scrollArea.SetContent(o.Table(), unison.FillBehavior, unison.FillBehavior)
-	scrollArea.SetLayoutData(&unison.FlexLayoutData{
-		HAlign: unison.FillAlignment,
-		VAlign: unison.FillAlignment,
-		HGrab:  true,
-		VGrab:  true,
-	})
-	content.AddChild(scrollArea)
 	return true
 }
